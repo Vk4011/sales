67a6eb53c371a3a339b5261a282e391ffacfb902
Vk4011 <maticsystem1@gmail.com>
2025-04-03 15:47:42 +0530

    api

--- demo/src/main/resources/static/js/login.js
+++ demo/src/main/resources/static/js/login.js
@@ -14,7 +14,7 @@ async function validateLogin() {
     }
 
     try {
-        const response = await fetch('http://localhost:6060/api/login', {
+        const response = await fetch('https://sales-fi3s.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
